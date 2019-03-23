2c366cf9f5659681f1a75f4a9828933cf3c7db15
leiserfg <leiserfg@gmail.com>
2019-03-23 18:34:23 +0100

    Fix structure and add events

--- rplugin/python3/qalc/__init__.py
+++ rplugin/python3/qalc/__init__.py
@@ -1,13 +1,14 @@
 import pynvim
-from qalc import process
-
+from .qalc import process
+from .util import throttle
 
 @pynvim.plugin
 class QalcPlugin:
     def __init__(self, nvim):
         self.nvim = nvim
  
-    @pynvim.autocmd('BufWritePost', pattern='*.calc')
-    def on_bufenter(self, filename):
+    @pynvim.autocmd('BufNewFile,BufRead,TextChanged,TextChangedI', pattern='*.qalc')
+    @throttle(0.3)
+    def on_bufenter(self):
         for i, v in process(self.nvim.current.buffer):
             self.nvim.call('nvim_buf_set_virtual_text', self.nvim.current.buffer, 0, i, [[v, 'Error']], {})
