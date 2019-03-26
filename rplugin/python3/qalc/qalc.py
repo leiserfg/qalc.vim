4903574659b8b04c03e18b656e96fb6136a1b46c
leiserfg <leiserfg@gmail.com>
2019-03-26 19:54:05 +0100

    Fix output style

--- rplugin/python3/qalc/qalc.py
+++ rplugin/python3/qalc/qalc.py
@@ -1,13 +1,22 @@
 import re
+from functools import reduce
 from subprocess import PIPE, run
+
 from .util import ansi_escape
 
-_comment_re = re.compile("#.*$", re.MULTILINE)
+_comment_re = re.compile("\s*#.*$", re.MULTILINE)
+_label_re = re.compile("^.*:", re.MULTILINE)
+
 _prompt_or_empty_re = re.compile("^(>.*|\s+)$", re.MULTILINE)
+_translate = [("deg", "°"), (" USD", "$"), (" EUR", "€"), (" GBP", "£"), (" JPY", "¥")]
 
 
 def clean_input(input):
-    lines = ((i, _comment_re.sub("", line)) for i, line in enumerate(input) if line)
+    uncommented_lines = (_comment_re.sub("", line) for line in input)
+    uncommented_lines = (_label_re.sub("", line) for line in uncommented_lines)
+    lines = (
+        (i, l) for i, l in enumerate(uncommented_lines) if l
+    )  # clear empty lines and comment lines
     idxs = []
     new_text = []
     for i, l in lines:
@@ -25,25 +34,14 @@ def clean_output(output):
     without_prompt = _prompt_or_empty_re.sub("", output)
     for line in without_prompt.splitlines():
         if line:
-            yield ansi_escape(line)
+            without_ansi = ansi_escape(line)
+            formated = reduce(
+                lambda val, x: val.replace(x[0], x[1]), _translate, without_ansi
+            )
+            yield formated.strip()
 
 
 def process(input):
     idxs, text = clean_input(input)
     output = qalc(text)
     return zip(idxs, clean_output(output))
-
-
-def main():
-    text = """
-    343 + 3434
-    33 + sin(12)
-
-    12USD to EUR
-    asdfasdf  #adsfasdf
-    """
-    print("\n".join(f"{id} {val}" for id, val in process(text)))
-
-
-if __name__ == "__main__":
-    main()
